834290a511b4c12d97a490fd2483706dbb8eaa31
yvonnkr <34454562+yvonnkr@users.noreply.github.com>
2021-02-10 23:07:31 +0000

    renamed main

--- ppmtool/src/main/java/com/yvolabs/ppmtool/Main.java
+++ ppmtool/src/main/java/com/yvolabs/ppmtool/Main.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PpmtoolApplication {
+public class Main {
 
     public static void main(String[] args) {
-        SpringApplication.run(PpmtoolApplication.class, args);
+
+        SpringApplication.run(Main.class, args);
     }
 
 }
