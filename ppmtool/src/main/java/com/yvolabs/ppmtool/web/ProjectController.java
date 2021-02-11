da680a2385a9a0a42267840e5c33240de4880b9b
yvonnkr <34454562+yvonnkr@users.noreply.github.com>
2021-02-11 13:52:36 +0000

    validate createNewProject and map field errors

--- ppmtool/src/main/java/com/yvolabs/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/com/yvolabs/ppmtool/web/ProjectController.java
@@ -5,10 +5,15 @@ import com.yvolabs.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/project")
@@ -22,7 +27,17 @@ public class ProjectController {
 
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+       if(result.hasErrors()){
+           Map<String, String> errorMap = result.getFieldErrors()
+                   .stream()
+                   .collect(Collectors.toMap(e -> e.getField(), e -> e.getDefaultMessage()));
+
+
+           return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
+
+       }
+
         Project newProject = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<>(newProject, HttpStatus.CREATED);
     }
