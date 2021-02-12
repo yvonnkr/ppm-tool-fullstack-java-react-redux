18f953c1beb1f76fa361f2b6b72872f64f8bbb52
yvonnkr <34454562+yvonnkr@users.noreply.github.com>
2021-02-12 12:19:06 +0000

    html/css for Header,Dashboard,ProjectItem components

--- ppmtool-client/src/App.js
+++ ppmtool-client/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
-import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/layout/Header";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   return (
