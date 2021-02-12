e74a4ea85f4beecbf582754581a747903f11a28b
yvonnkr <34454562+yvonnkr@users.noreply.github.com>
2021-02-12 20:33:51 +0000

    Created Redux Store

--- ppmtool-client/src/App.js
+++ ppmtool-client/src/App.js
@@ -1,20 +1,26 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+
 import Dashboard from "./components/Dashboard";
 import Header from "./components/layout/Header";
 import AddProject from "./components/project/AddProject";
+import store from "./store";
+
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Header />
-        <Route exact path="/dashboard" component={Dashboard} />
-        <Route exact path="/addProject" component={AddProject} />
-      </div>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="App">
+          <Header />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/addProject" component={AddProject} />
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
 };
 
