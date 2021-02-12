7bab8d3dc9e098ba13ab8c98d0d0159dfb3da476
yvonnkr <34454562+yvonnkr@users.noreply.github.com>
2021-02-12 18:33:45 +0000

    Routing, AddProject Component

--- ppmtool-client/src/App.js
+++ ppmtool-client/src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/layout/Header";
+import AddProject from "./components/project/AddProject";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   return (
-    <div className="App">
-      <Header />
-      <Dashboard />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/addProject" component={AddProject} />
+      </div>
+    </BrowserRouter>
   );
 };
 
--- ppmtool-client/src/components/Dashboard.js
+++ ppmtool-client/src/components/Dashboard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ProjectItem from "./project/ProjectItem";
+import CreateProjectButton from "./project/CreateProjectButton";
 
 const Dashboard = () => {
   return (
@@ -9,9 +10,9 @@ const Dashboard = () => {
           <div className="col-md-12">
             <h1 className="display-4 text-center">Projects</h1>
             <br />
-            <a href="ProjectForm.html" className="btn btn-lg btn-info">
-              Create a Project
-            </a>
+
+            <CreateProjectButton />
+
             <br />
             <hr />
             <ProjectItem />
