820a5ecf337f4c39209905ca325fc7195d6830a3
yvonnkr <34454562+yvonnkr@users.noreply.github.com>
2021-02-13 12:53:55 +0000

    server allow cors

--- ppmtool/src/main/java/com/yvolabs/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/com/yvolabs/ppmtool/web/ProjectController.java
@@ -13,6 +13,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("api/project")
+@CrossOrigin("*")
 public class ProjectController {
     private final MapValidationErrorService mapValidationErrorService;
     private final ProjectService projectService;
