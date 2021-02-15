4ba2821a1d23209cbeb5dbe6d2b2d1a5d0599da9
yvonnkr <34454562+yvonnkr@users.noreply.github.com>
2021-02-15 13:24:16 +0000

    Repositories Backlog and ProjectTask

---
+++ ppmtool/src/main/java/com/yvolabs/ppmtool/repositories/ProjectTaskRepository.java
@@ -0,0 +1,9 @@
+package com.yvolabs.ppmtool.repositories;
+
+import com.yvolabs.ppmtool.domain.ProjectTask;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectTaskRepository extends CrudRepository<ProjectTask, Long> {
+}
