767f1f25bcd13aab3708ea0e979deb6459deeaa5
yvonnkr <34454562+yvonnkr@users.noreply.github.com>
2021-02-15 19:17:15 +0000

    ProjectTaskService and persist, Backlog controller

---
+++ ppmtool/src/main/java/com/yvolabs/ppmtool/services/ProjectTaskService.java
@@ -0,0 +1,55 @@
+package com.yvolabs.ppmtool.services;
+
+import com.yvolabs.ppmtool.domain.Backlog;
+import com.yvolabs.ppmtool.domain.ProjectTask;
+import com.yvolabs.ppmtool.repositories.BacklogRepository;
+import com.yvolabs.ppmtool.repositories.ProjectTaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectTaskService {
+    private BacklogRepository backlogRepository;
+    private ProjectTaskRepository projectTaskRepository;
+
+    @Autowired
+    public ProjectTaskService(BacklogRepository backlogRepository, ProjectTaskRepository projectTaskRepository) {
+        this.backlogRepository = backlogRepository;
+        this.projectTaskRepository = projectTaskRepository;
+    }
+
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
+        // PTs to be added to a specific project (project != null meaning backlog(BL) also exists)
+        // todo: handle project not found  exceptions
+
+        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+
+        //set the BL to the PT
+        projectTask.setBacklog(backlog);
+
+        //sequence = (projectIdentifier + PTSequence)
+
+        // Get BL sequence
+        Integer backlogSequence = backlog.getPTSequence();
+
+        // Update the BL sequence
+        backlogSequence++;
+
+        // Add sequence to Project Task
+        projectTask.setProjectSequence(projectIdentifier + "-" + backlogSequence);
+        projectTask.setProjectIdentifier(projectIdentifier);
+
+        //Initial priority when priority is null
+//        if(projectTask.getPriority() == 0 || projectTask.getPriority() == null){
+//            projectTask.setPriority(3);
+//        }
+        //Initial status when status is null
+        if(projectTask.getStatus() == "" || projectTask.getStatus() == null){
+            projectTask.setStatus("TO_DO");
+            // could use enums instead
+        }
+
+        return projectTaskRepository.save(projectTask);
+
+    }
+}
---
+++ ppmtool/src/main/java/com/yvolabs/ppmtool/web/BacklogController.java
@@ -0,0 +1,40 @@
+package com.yvolabs.ppmtool.web;
+
+import com.yvolabs.ppmtool.domain.ProjectTask;
+import com.yvolabs.ppmtool.services.MapValidationErrorService;
+import com.yvolabs.ppmtool.services.ProjectTaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("api/backlog")
+@CrossOrigin("*")
+public class BacklogController {
+    private final MapValidationErrorService mapValidationErrorService;
+    private ProjectTaskService projectTaskService;
+
+    @Autowired
+    public BacklogController(MapValidationErrorService mapValidationErrorService, ProjectTaskService projectTaskService) {
+        this.mapValidationErrorService = mapValidationErrorService;
+        this.projectTaskService = projectTaskService;
+    }
+
+    @PostMapping("/{backlog_id}")
+    public ResponseEntity<?> addProjectTaskToBacklog(@Valid
+                                                     @RequestBody ProjectTask projectTask,
+                                                     BindingResult result,
+                                                     @PathVariable("backlog_id") String backlog_id) {
+
+        ResponseEntity<?> errorMap = mapValidationErrorService.mapValidationErrorResult(result);
+        if (errorMap != null) return errorMap;
+
+        ProjectTask newProjectTask = projectTaskService.addProjectTask(backlog_id, projectTask);
+        return new ResponseEntity<>(newProjectTask, HttpStatus.CREATED);
+
+    }
+}
