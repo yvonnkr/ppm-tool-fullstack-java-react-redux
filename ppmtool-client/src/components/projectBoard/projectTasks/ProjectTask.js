14fe47f011ef4ed403ad41d349a63c633ecf0a12
yvonnkr <34454562+yvonnkr@users.noreply.github.com>
2021-02-17 15:18:17 +0000

    Components Backlog and ProjectTask, refactor

--- ppmtool-client/src/components/projectBoard/projectTasks/ProjectTask.js
+++ ppmtool-client/src/components/projectBoard/projectTasks/ProjectTask.js
@@ -2,8 +2,21 @@ import React from "react";
 
 const ProjectTask = () => {
   return (
-    <div>
-      <h1>Project Task Item</h1>
+    <div className="card mb-1 bg-light">
+      <div className="card-header text-primary">
+        ID: projectSequence -- Priority: priorityString
+      </div>
+      <div className="card-body bg-light">
+        <h5 className="card-title">project_task.summary</h5>
+        <p className="card-text text-truncate ">
+          project_task.acceptanceCriteria
+        </p>
+        <a href="" className="btn btn-info">
+          View / Update
+        </a>
+
+        <button className="btn btn-danger ml-4">Delete</button>
+      </div>
     </div>
   );
 };
