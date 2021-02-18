593d0abfcd50221f7bdc10956749c50b8b76eec6
yvonnkr <34454562+yvonnkr@users.noreply.github.com>
2021-02-18 17:41:48 +0000

    Register new User

--- ppmtool/src/main/java/com/yvolabs/ppmtool/security/SecurityConfig.java
+++ ppmtool/src/main/java/com/yvolabs/ppmtool/security/SecurityConfig.java
@@ -45,6 +45,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/**/*.css",
                         "/**/*.js"
                 ).permitAll()
+                .antMatchers("/api/users/**").permitAll()
                 .anyRequest().authenticated();
 
     }
--- ppmtool/src/main/java/com/yvolabs/ppmtool/services/UserService.java
+++ ppmtool/src/main/java/com/yvolabs/ppmtool/services/UserService.java
@@ -1,15 +1,30 @@
 package com.yvolabs.ppmtool.services;
 
+import com.yvolabs.ppmtool.domain.User;
 import com.yvolabs.ppmtool.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public User saveUser (User newUser){
+        String encodedPassword = bCryptPasswordEncoder.encode(newUser.getPassword());
+        newUser.setPassword(encodedPassword);
+
+        // todo: Username has to be unique (exception)
+
+        // todo: Make sure that password and confirmPassword match
+        // todo: We don't persist or show the confirmPassword
+        return userRepository.save(newUser);
     }
 }
